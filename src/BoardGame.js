b6376ea4861ea824669067d4afcf6ba55375ba60
Anna Peterson <reveragb@gmail.com>
2022-09-16 10:24:45 -0600

    Finish boardgame tests

--- src/BoardGame.js
+++ src/BoardGame.js
@@ -1,5 +1,18 @@
 class BoardGame {
-
+    constructor(game) {
+        this.name = game.name;
+        this.rating = game.rating;
+        this.minimumPlayers = game.minPlayers;
+        this.maximumPlayers = game.maxPlayers;
+        this.played = game.played;
+    }
+    play() {
+        if (this.rating === 10) {
+            this.played = true;
+        } else {
+            this.played = false;
+        }
+    }
 }
 
 module.exports = BoardGame;
