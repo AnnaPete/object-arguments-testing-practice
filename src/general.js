122ec3e004669dde2d26ea5ca53fd7d89e9250aa
Anna Peterson <reveragb@gmail.com>
2022-09-16 11:03:08 -0600

    Finish general tests, spicy filter scores test

--- src/general.js
+++ src/general.js
@@ -1,8 +1,27 @@
 // you'll need to comment out the corresponding function below when you write it.
 
+function getFavoriteFood(foodOpinions) {
+  return foodOpinions.favoriteFood;
+}
+function findDifference(moneyStillNeeded) {
+  return moneyStillNeeded.ipadCost - moneyStillNeeded.moneySaved;
+}
+function listFavoriteDestinations(destinations) {
+  return destinations.favoriteDestinations;
+}
+function filterScores(subject, scores) {
+  for (var i = 0; i < scores[subject].length; i++) {
+    if (scores[subject][i] < 70) {
+      scores[subject].splice(i, 1);
+      i--
+    }
+  }
+  return scores[subject];
+}
+
 module.exports = {
-  // getFavoriteFood,
-  // findDifference,
-  // listFavoriteDestinations,
-  // filterScores,
+  getFavoriteFood,
+  findDifference,
+  listFavoriteDestinations,
+  filterScores,
 }
